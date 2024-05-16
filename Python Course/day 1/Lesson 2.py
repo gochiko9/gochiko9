5170f7c5825e40d8ae38f6bdcba94ad1130a81c5
gochiko9 <shukakidzenino77@gmail.com>
2024-05-16 22:48:37 +0400

    Add files via upload

---
+++ Python Course/day 1/Lesson 2.py
@@ -0,0 +1,22 @@
+name = "gocha"
+#name არის ცვლადი
+
+surname = "wuriasvhili"
+
+name = "gocha" #ეს არის str (string) ტიპის ცვლადი
+
+age = 9  # ეს არის int (integer) მთელი რიცხვი
+
+height = 130 #ეს არის float ცვლადი ( ათწილადი )
+
+knows_programming = True
+
+is_ugly = False
+
+print(name + " " + surname ) 
+
+print(name +" " + str (age))
+
+print(type(age))
+print(name + surname+ " " + " " +str(age)" " + str(height))
+ 
